0fc3ec162232294b54ebe9fddbf6a2002de037c1
peterselah <108258619+peterselah@users.noreply.github.com>
2022-06-27 10:11:51 +0300

    Create 27-07-2022.py

---
+++ 27-07-2022.py
@@ -0,0 +1,21 @@
+'''
+role du flux.  ecris utilisant “if zzzzzzz” imprimer “x is less than y”.
+ 
+'''
+
+x = 300
+
+'''
+je ecris un note pour moi
+'''
+
+y = 300
+if (x<y):
+  isSmaller=x < y
+  print("x c'est moine que y", isSmaller,  " x = " , x , " y =", y)
+else:
+  print("x est plus grand que y ou x equal y",  x >= y)
+
+'''
+que ce quil pas si x=y?
+'''
