ade1668ef693b746ca9773e77cb34a89f4fb9ac3
peterselah <108258619+peterselah@users.noreply.github.com>
2022-07-07 09:27:41 +0300

    Create 07-07-2022.py

---
+++ 07-07-2022.py
@@ -0,0 +1,24 @@
+animaux = ["giraf", "tigre", "singe", "souris"]
+
+for i in range(0,10,2):
+  print(i)
+  
+  '''
+ 
+
+len(animaux)<------utilize cetter nombre pour savoir quand tu dois terminer.  vous pourriez mettre le numéro 4 ici. mais c'est mauvais. c'est ce qu'on appelle le codage en dur. à la place, mettez len(liste).
+
+
+start=0  <   commencer par la fin 
+end=?
+step=?   doit etre -1
+
+for i in range(start,end,step):
+   print something
+'''
+
+animaux = ["giraf", "tigre", "singe", "souris"]
+for animaux in range():  <--c'est rien
+print(animaux)
+
+ 
